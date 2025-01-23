f5e855d88dbd9265c27b32df13ccda5af1dbfe06
josselrempis02 <josselrempis02@gmail.com>
2025-01-23 16:14:29 +0800

    add jon

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import Joblistings from "./components/Joblistings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+//
+// import Hero from "./components/Hero";
+// import HomeCards from "./components/HomeCards";
+// import Joblistings from "./components/Joblistings";
+// import ViewAllJobs from "./components/ViewAllJobs";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+    </Route>
+  )
+);
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <Joblistings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
-
 export default App;
--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { FaMapMarked } from "react-icons/fa";
 
 const Joblisting = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -9,6 +10,11 @@ const Joblisting = ({ job }) => {
   if (!showFullDescription) {
     description = description.substring(0, 90) + "...";
   }
+
+  const see = () => {
+    setShowFullDescription((prevState) => !prevState);
+  };
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -20,7 +26,7 @@ const Joblisting = ({ job }) => {
         <div className="mb-5">{description}</div>
 
         <button
-          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          onClick={see}
           className="text-indigo-500 mb-5 hover:text-indigo-600"
         >
           {showFullDescription ? "Less" : "More"}
@@ -32,7 +38,7 @@ const Joblisting = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarked className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <a
